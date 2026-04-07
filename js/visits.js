b3ae7df0f83c2273f7a7e00ee8bdb379959396f9
Cesar Sobrino <cesassab@gmail.com>
2026-04-07 11:45:56 +0200

    update visits

--- js/visits.js
+++ js/visits.js
@@ -1,10 +1,36 @@
 (function () {
     console.log("v1");
-    const VISIT_ENDPOINT = "https://cesarsobapi-csa-workspace.up.railway.app/api/Visits/addvisit";
+    const VISIT_ENDPOINT = "https://api.cesarsobrino.es/api/Visits/addvisit";
+    // const VISIT_ENDPOINT = "http://localhost:5112/api/Visits/addvisit";
+
+    function getVisitParam() {
+        const queryParam = new URLSearchParams(window.location.search).get("visitparams");
+        if (queryParam) {
+            return queryParam;
+        }
+
+        const segments = window.location.pathname.split("/").filter(Boolean);
+        const visitParamsIndex = segments.findIndex(function (segment) {
+            return segment.toLowerCase() === "visitparams";
+        });
+
+        if (visitParamsIndex !== -1 && segments[visitParamsIndex + 1]) {
+            return decodeURIComponent(segments[visitParamsIndex + 1]);
+        }
+
+        return null;
+    }
 
 async function registerVisit() {
     try {
-        await fetch(VISIT_ENDPOINT, {
+        const visitParam = getVisitParam();
+        const endpointUrl = new URL(VISIT_ENDPOINT);
+
+        if (visitParam) {
+            endpointUrl.searchParams.set("visitparams", visitParam);
+        }
+
+        await fetch(endpointUrl.toString(), {
             method: "POST",
             mode: "cors",
             credentials: "omit",
